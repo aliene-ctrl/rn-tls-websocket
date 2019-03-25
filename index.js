981e5b4f1db9bcf4747509008d74cbb9d5dbcb26
Ian Brindley <ianbrindley1@gmail.com>
2019-03-25 21:35:48 +0000

    wip

--- index.js
+++ index.js
@@ -3,4 +3,7 @@ import { NativeModules } from 'react-native';
 
 const { RNUnsafeWebSocket } = NativeModules;
 
+console.log('@@@@@@@@@@@@@@@@@@');
+console.log(NativeModules);
+
 export default RNUnsafeWebSocket;
